9e883af798283c30e81aa47849a762ce7dc9a33e
Isaac <isaacgeazy@gmail.com>
2021-03-12 22:39:57 -0300

    Atividade Milhas para KM

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _12_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+               string entrada;
+               double km = 1.609, resu, milhas;
+
+            Console.ForegroundColor = ConsoleColor.Blue;        
+            Console.WriteLine("--------------Conversão de milhas para KM-------------");
+            Console.WriteLine();
+            Console.ResetColor();
+            Console.Write("Digite o valor em milhas: ");
+            entrada = Console.ReadLine();
+
+            milhas = Convert.ToDouble(entrada);
+            resu = milhas * km;
+            Console.WriteLine();
+            Console.WriteLine($"{milhas} MILHAS para {resu:N3} KM");
+            
+
+            
+
+            
+           
+
+
+
+        }
+    }
+}
